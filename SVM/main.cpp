739389738042e18c1ba672c9705a7c7cfbc425b4
LIN Pan <pandolphlin@gmail.com>
2018-03-28 15:32:48 +0800

    Initial Commit

---
+++ SVM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SVM
+//
+//  Created by Pan on 2018/3/28.
+//  Copyright © 2018年 Pan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
